354a75bc9ee0b2e26e8caf78ae43c2d76270af86
Owner <owner@OwnernoMacBook-Pro.local>
2020-04-22 16:04:30 +0900

    feat(create.component):フォームの実装

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreateRoutingModule } from './create-routing.module';
-
+import { CreateComponent } from './create/create.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateComponent],
   imports: [
     CommonModule,
-    CreateRoutingModule
-  ]
+    CreateRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatRadioModule,
+  ],
 })
-export class CreateModule { }
+export class CreateModule {}
