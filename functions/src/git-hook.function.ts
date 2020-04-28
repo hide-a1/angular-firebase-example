39e752245d1c5e5773ff4af322d7118ceddc865f
Owner <owner@OwnernoMacBook-Pro.local>
2020-04-28 14:41:42 +0900

    feat(git-hook):レベル計算追加

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -6,13 +6,41 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const expTable = [
+  20,
+  40,
+  100,
+  250,
+  500,
+  1000,
+  1500,
+  4000,
+  10000,
+];
+
+const EARN_EXPERIENCE = 10;
+
 export const gitHook = functions.https.onRequest(async (request, response) => {
   const pets = await db.collection('pets')
     .where('ownerGitHubId', '==', request.body.sender.id)
-    .get()
+    .get();
+
+  const pet = pets.docs[0].data();
+
+  let level = 1;
+  expTable.some(nextExp => {
+    if (pet.exp + EARN_EXPERIENCE >= nextExp) {
+      level++;
+      return false;
+    } else {
+      return true;
+    }
+  });
+
   const increement = admin.firestore.FieldValue.increment(10);
   pets.docs.forEach(doc => doc.ref.update({
-    exp: increement
+    exp: increement,
+    level
   }));
   response.send('success');
 });
